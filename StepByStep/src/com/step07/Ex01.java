510387f144fb94a2cd1bb85a085917156fd9af47
iamleechanmi <leechanmi@kakao.com>
2021-01-21 23:45:21 +0900

    Solve question

---
+++ StepByStep/src/com/step07/Ex01.java
@@ -0,0 +1,14 @@
+package com.step07;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		//아스키 코드
+		Scanner sc = new Scanner(System.in);
+		System.out.println((int)sc.next().charAt(0));
+		sc.close();
+	}
+}
